bb4db3b92a8c7f2c81d6e43c88a5fb7a54e364ce
Min <minkim.dev@gmail.com>
2018-06-19 23:24:43 -0400

    adding model and database connection through synchronizing them with sequelize orm

---
+++ migrations/20180620031403-create-todo.js
@@ -0,0 +1,27 @@
+'use strict';
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    return queryInterface.createTable('Todos', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      body: {
+        type: Sequelize.STRING
+      },
+      complete: {
+        allowNull: false,
+        type: Sequelize.INTEGER
+      },
+      created: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  down: (queryInterface, Sequelize) => {
+    return queryInterface.dropTable('Todos');
+  }
+};
