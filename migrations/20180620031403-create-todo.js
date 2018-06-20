3451b86606d851adeb2af79cad09f2cf80b57549
Min <minkim.dev@gmail.com>
2018-06-20 00:23:31 -0400

    change the requirements

--- migrations/20180620031403-create-todo.js
+++ migrations/20180620031403-create-todo.js
@@ -1,7 +1,7 @@
 'use strict';
 module.exports = {
   up: (queryInterface, Sequelize) => {
-    return queryInterface.createTable("Todos", {
+    return queryInterface.createTable("Todo", {
       id: {
         allowNull: false,
         autoIncrement: true,
