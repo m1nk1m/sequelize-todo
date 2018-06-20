47daae36a1c4ddfbf5fe3d3cd65b3bce7b694907
Min <minkim.dev@gmail.com>
2018-06-19 23:24:26 -0400

    adding model and database connection through synchronizing them with sequelize orm

--- models/todo.js
+++ models/todo.js
@@ -2,7 +2,7 @@
 module.exports = (sequelize, DataTypes) => {
   var Todo = sequelize.define('Todo', {
     body: DataTypes.STRING,
-    complete: DataTypes.NUMBER,
+    complete: DataTypes.INTEGER,
     created: DataTypes.DATE
   }, {});
   Todo.associate = function(models) {
