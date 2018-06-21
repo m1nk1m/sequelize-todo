4509f7b2cd4f80600a7bf341fbec677d205e9d69
Min <minkim.dev@gmail.com>
2018-06-21 03:40:41 -0400

    adding markdown for instructions

--- app.js
+++ app.js
@@ -10,15 +10,10 @@ const createError = require('http-errors'),
   todoRouter = require('./routes/todos'),
   app = express();
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
-
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
 app.use(cors());
 
 app.use('/', indexRouter());
